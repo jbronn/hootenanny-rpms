f5856c3559c4b60a11c6a513768d6c6b07acfa2f
Cellington1 <codyellington@gmail.com>
2018-03-19 12:12:32 -0400

    Adding entrypoint back in

--- shell/Vars.sh
+++ shell/Vars.sh
@@ -283,7 +283,8 @@ function run_hoot_build_image() {
                -v $CACHE/m2:/rpmbuild/.m2:rw \
                -v $CACHE/npm:/rpmbuild/.npm:rw \
                -v $SCRIPT_HOME/scripts:/rpmbuild/scripts:ro \
-               -u $user \
+               --entrypoint $entrypoint \
+	       -u $user \
 	       -i --rm \
                $image "${@:-/bin/bash}"
     fi
